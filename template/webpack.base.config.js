fecdfd26a31a6e85579a75819777f1ac7dd649ed
mrgao <gaogangwork@qq.com>
2019-04-17 14:58:54 +0800

    添加分类打包

--- template/webpack.base.config.js
+++ template/webpack.base.config.js
@@ -67,7 +67,27 @@ module.exports = {
       {
         //此处配置为iview的注意点，如果不配置的话 无法再Js文件中加载iview.css文件；其次如果使用url-loader无法加载的话，会使用file-loader进行文件加载
         test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
-        loader: "url-loader?limit=1024"
+        loader: "url-loader?limit=1024",
+        options:{
+          limit: 10000,
+          name: 'img/[name].[hash].[ext]'
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'media/[name].[hash].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[hash].[ext]'
+        }
       },
       {
         test: /\.(html|tpl)$/,
