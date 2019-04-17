0cbabcb78b2e0d60eaf56cfa8f9282b149b34881
mrgao <gaogangwork@qq.com>
2019-04-17 15:02:27 +0800

    Update webpack.base.config.js

--- template/webpack.base.config.js
+++ template/webpack.base.config.js
@@ -10,8 +10,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, "./dist"), //指定输出目录
-    libraryTarget: "umd",
-    filename: "main.js" //输出文件
+    libraryTarget: "umd"
   },
   stats: { children: false },
   module: {
