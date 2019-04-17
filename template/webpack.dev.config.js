5bba6ecb57ac457e4414ee964bb23ea426801612
mrgao <gaogangwork@qq.com>
2019-04-17 14:24:52 +0800

    Update webpack.dev.config.js

--- template/webpack.dev.config.js
+++ template/webpack.dev.config.js
@@ -2,15 +2,21 @@ var merge = require("webpack-merge");
 var baseConfig = require("./webpack.base.config.js");
 var VueLoaderPlugin = require("vue-loader/lib/plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
+var HtmlwebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
 baseConfig.plugins = [];
 module.exports = merge(baseConfig, {
-    plugins: [
-        new VueLoaderPlugin(),
-        //将css文件合并为main.css
-        new ExtractTextPlugin({
-            filename: "main.css",
-            allChunks: true
-        }),
-    ]
-})
+  output: {
+    publicPath: 'dist/',
+  },
+  devtool: '#eval-source-map',//开启测试环境调试
+  plugins: [
+
+    new VueLoaderPlugin(),
+    //将css文件合并为main.css
+    new ExtractTextPlugin({
+      filename: "main.css",
+      allChunks: true
+    })
+  ]
+});
