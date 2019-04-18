a25f94eaa1f0b6480c9a8387d2491d5fafaf53d8
mrgao <gaogangwork@qq.com>
2019-04-18 16:18:32 +0800

    Update home.js

--- template/src/server/modules/home.js
+++ template/src/server/modules/home.js
@@ -4,6 +4,7 @@ import {
 import fetch from "../fetch";
 export default {
   getPersonalInfo(userName) {
+    //可以使用  return fetch(HOME.userInfo + userName,{useJsonp:true});使用jsonp实现跨域请求
     return fetch(HOME.userInfo + userName);
   }
 }
