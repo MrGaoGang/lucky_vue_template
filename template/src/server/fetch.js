ca97b2cb282988a69de37370a1518b6ec791e8e9
mrgao <gaogangwork@qq.com>
2019-04-18 16:15:14 +0800

    添加跨域jsonp实现get请求

--- template/src/server/fetch.js
+++ template/src/server/fetch.js
@@ -5,11 +5,10 @@ const METHODS = {
 
 const defaultParmas = {
   headers: {
-    'content-type': 'application/json'
+    "content-type": "application/json"
   },
-  method: METHODS.GET, // *GET, POST, PUT, DELETE, etc.
+  method: METHODS.GET // *GET, POST, PUT, DELETE, etc.
 };
-
 
 /**
  * 简单封装了一下网络请求
@@ -29,28 +28,49 @@ const defaultParmas = {
     referrer: 'no-referrer', // *client, no-referrer
   }
  */
-export default function (url, method = METHODS.GET, data = {}) {
-
+export default function(
+  url,
+  { method = METHODS.GET, data = {}, useJsonp = false } = {}
+) {
   let params = defaultParmas;
   if (method == METHODS.POST) {
-    params.method = METHODS.POST
+    params.method = METHODS.POST;
     params.data = JSON.stringify(data);
   }
 
-
   return new Promise((resolve, reject) => {
-    fetch(url, params)
-      .then(response => {
-        if (response.status == 200) {
-          return response.json();
-        } else {
-          return JSON.stringify({});
-        }
-      })
-      .then(data => {
-        resolve(data)
-      }).catch(error => {
-        reject(error);
-      })
-  })
+    if (method == METHODS.GET && useJsonp) {
+      //jsonp实现跨域get请求
+      let script = document.createElement("script");
+      let callbackName = "jsonp_callback";
+      if (url.indexOf("?") != -1) {
+        url += "&callback=" + callbackName;
+      } else {
+        url += "?callback=" + callbackName;
+      }
+      script.src = url;
+      window[callbackName] = function(data) {
+        //使用json方式请求，返回的数据结构为:{data: {},meta:{}}
+        resolve(JSON.stringify(data.data));
+        document.body.removeChild(script);
+      };
+      document.body.appendChild(script);
+    } else {
+      //使用默认的fetch请求
+      fetch(url, params)
+        .then(response => {
+          if (response.status == 200) {
+            return response.json();
+          } else {
+            return JSON.stringify({});
+          }
+        })
+        .then(data => {
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    }
+  });
 }
